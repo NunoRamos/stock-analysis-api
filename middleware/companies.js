d2fba9edab71888154ad1aaac15f21b00119518d
Nuno Ramos <nunor07.unfc@gmail.com>
2018-12-12 21:54:19 +0000

    Route companies/daily-quote/:companiesList done!

--- middleware/companies.js
+++ middleware/companies.js
@@ -1,11 +1,30 @@
+const requestPromise = require('request-promise');
+
 exports.getCompaniesQuotes = (req, res, next) => {
-    let companies = req.params.companies;
-    console.log("Params: " + req.params);
-    console.log("Companies: " + companies);
-    /*companies.forEach(element => {
-        console.log(element);
-    });*/
+    let companies = req.params.companiesList;
     let companiesArray = JSON.parse(companies);
+
+    const promiseCalls = [];
+    let prices = [];
+
+    for (index in companiesArray)
+        promiseCalls.push(this.getCompanyQuote(companiesArray[index]))
     
-    return res.status(200).json(companies);
+    Promise.all(promiseCalls).then(results => {
+        results.forEach(quote => {
+            prices.push(quote);
+        });
+        return res.status(200).json(prices);
+    });
 };
+
+exports.getCompanyQuote = (companySymbol) => new Promise(resolve => {
+    requestPromise('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=' + companySymbol + '&apikey=0V27RLXB3DH4EYVO')
+        .then(function (htmlString) {
+           let json = JSON.parse(htmlString);
+           return resolve(json['Global Quote']);
+        })
+        .catch(function (err) {
+            return resolve({});
+        });
+});
--- routes/companies.js
+++ routes/companies.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 const companiesMiddleware = require('../middleware/companies');
 
-router.get('/daily-quote/:companies', companiesMiddleware.getCompaniesQuotes);
+router.get('/daily-quote/:companiesList', companiesMiddleware.getCompaniesQuotes);
 
 module.exports = router;
